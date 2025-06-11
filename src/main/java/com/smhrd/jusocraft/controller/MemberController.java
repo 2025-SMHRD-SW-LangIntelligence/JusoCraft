4b2025374be9b3c917ed0c17724489dd3f8af144
tnvs99 <tnvs99@gmail.com>
2025-06-11 11:23:10 +0900

    Create Project

---
+++ src/main/java/com/smhrd/jusocraft/controller/MemberController.java
@@ -0,0 +1,7 @@
+package com.smhrd.jusocraft.controller;
+
+public class MemberController {
+    public static void main(String[] args) {
+        System.out.println("주소크래프트");
+    }
+}
---
+++ src/test/java/com/smhrd/jusocraft/JusoCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smhrd.jusocraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JusoCraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

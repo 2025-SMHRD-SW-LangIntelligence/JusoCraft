03c17602b60a7b2937e00ca182ecc7a3be40a2ad
tnvs99 <tnvs99@gmail.com>
2025-06-11 11:23:37 +0900

    Create Project

--- src/main/java/com/smhrd/jusocraft/controller/MemberController.java
+++ src/main/java/com/smhrd/jusocraft/controller/MemberController.java
@@ -2,6 +2,6 @@ package com.smhrd.jusocraft.controller;
 
 public class MemberController {
     public static void main(String[] args) {
-        System.out.println("주소크래프트");
+        System.out.println("김영훈");
     }
 }

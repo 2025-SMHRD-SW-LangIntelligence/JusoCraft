390b939fedf7d50169525a0eda1ec9fca7728ccc
mhleee <minsnailee@gmail.com>
2025-06-11 14:30:10 +0900

    수정

--- src/main/java/com/smhrd/jusocraft/controller/MemberController.java
+++ src/main/java/com/smhrd/jusocraft/controller/MemberController.java
@@ -4,7 +4,7 @@ public class MemberController {
     public static void main(String[] args) {
         System.out.println("김영훈");
         System.out.println("김지우");
-        System.out.println("이민희");
+        System.out.println("이민희22222");
         System.out.println("이준섭");
         System.out.println("문세인");
     }

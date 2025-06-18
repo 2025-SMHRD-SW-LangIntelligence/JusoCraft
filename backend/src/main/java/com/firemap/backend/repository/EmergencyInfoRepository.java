9dea37adc158c938520688bd86efe7bf8fd7ade6
tnvs99 <tnvs99@gmail.com>
2025-06-18 09:33:06 +0900

    fix 오타수정

--- backend/src/main/java/com/firemap/backend/repository/EmergencyInfoRepository.java
+++ backend/src/main/java/com/firemap/backend/repository/EmergencyInfoRepository.java
@@ -1,8 +1,9 @@
 package com.firemap.backend.repository;
 
+import com.firemap.backend.entity.EmergencyInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmergencyInfoRepository extends JpaRepository<EmergencyInfoRepository, Long> {
+public interface EmergencyInfoRepository extends JpaRepository<EmergencyInfoEntity, Long> {
 }

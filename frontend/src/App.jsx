67ed2814a6c66e7704e2d38867f38a68b86cee6a
tnvs99 <tnvs99@gmail.com>
2025-06-20 14:30:03 +0900

    update 신고 관리

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,9 @@ import ChatBotPage from "./pages/ChatBotPage/ChatBotPage";
 import DashboardWrapper from "./pages/DashboardPage/DashboardWrapper";
 import FirefighterPage from "./pages/FirefighterPage/FirefighterPage";
 import WindMap from "./pages/WindMap";
+import AllReportsPage from "./pages/AllReportsPage";
+import ActiveDispatchPage from "./pages/ActiveDispatchPage.jsx"
+import CompletedReportsPage from "./pages/CompletedReportsPage.jsx";
 
 // 페이지
 function App() {
@@ -23,6 +26,9 @@ function App() {
         <Route path="/dashboard" element={<DashboardWrapper />} />
         <Route path="/firefighter" element={<FirefighterPage />} />
         <Route path="/fire-risk" element={<WindMap />} />
+        <Route path="/reports/all" element={<AllReportsPage />} />
+        <Route path="/reports/dispatched" element={<ActiveDispatchPage />} />
+        <Route path="/reports/completed" element={<CompletedReportsPage />} />
       </Routes>
     </Router>
   );

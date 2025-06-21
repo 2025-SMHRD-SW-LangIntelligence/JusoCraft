5bbaa5310eafb8a8ae11518c56b433f6ec187068
snail <minsnailee@gmail.com>
2025-06-21 11:46:06 +0900

    응급실 및 화상 진료 병원 조회용 쿼리 메서드 추가

--- backend/src/main/java/com/firemap/backend/repository/EmergencyInfoRepository.java
+++ backend/src/main/java/com/firemap/backend/repository/EmergencyInfoRepository.java
@@ -1,9 +1,17 @@
 package com.firemap.backend.repository;
 
 import com.firemap.backend.entity.EmergencyInfoEntity;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmergencyInfoRepository extends JpaRepository<EmergencyInfoEntity, Long> {
+    // 1도, 2도 화상용
+    List<EmergencyInfoEntity> findByIsEmergencyRoomOperating(String y);
+
+    // 3도, 4도 화상용
+    List<EmergencyInfoEntity> findByIsEmergencyRoomOperatingAndBurnCare(String y1, String y2);
 }
